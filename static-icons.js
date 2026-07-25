f3739532afe09192dd04c03b075bec8861b55100
enkornkorn-lgtm <enkornkorn@gmail.com>
2026-07-25 21:14:53 +0700

    Delete static-icons.js

--- static-icons.js
+++
@@ -1,9 +0,0 @@
-/* เติมไอคอน SVG ให้องค์ประกอบสแตติก (แท็บบน, เมนูล่าง, ปุ่ม) ที่มี data-ic */
-(function fillStaticIcons(){
-  if(typeof window.skeIcon!=='function'){return setTimeout(fillStaticIcons,30);}
-  document.querySelectorAll('[data-ic]').forEach(function(el){
-    if(el.getAttribute('data-ic-filled'))return;
-    el.innerHTML=window.skeIcon(el.getAttribute('data-ic'));
-    el.setAttribute('data-ic-filled','1');
-  });
-})();
